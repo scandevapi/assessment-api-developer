f1e90500ebfdea313fa7f16a2750f6b2b40f23c8
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 09:43:26 -0700

    Adding Countries enum

---
+++ assessment-api-developer.Domain/Models/Countries.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace assessment_api_developer.Domain.Models
+{
+    public enum Countries
+    {
+        Canada,
+        [Description("United States")]
+        UnitedStates
+    }
+}
