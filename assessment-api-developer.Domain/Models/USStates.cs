5b00a15333af747593f5fefe395969b015bbcfe3
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 09:45:56 -0700

    Adding USStates enum

---
+++ assessment-api-developer.Domain/Models/USStates.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+
+namespace assessment_api_developer.Domain.Models
+{
+    public enum USStates
+    {
+        Alabama,
+        Alaska,
+        Arizona,
+        Arkansas,
+        California,
+        Colorado,
+        Connecticut,
+        Delaware,
+        Florida,
+        Georgia,
+        Hawaii,
+        Idaho,
+        Illinois,
+        Indiana,
+        Iowa,
+        Kansas,
+        Kentucky,
+        Louisiana,
+        Maine,
+        Maryland,
+        Massachusetts,
+        Michigan,
+        Minnesota,
+        Mississippi,
+        Missouri,
+        Montana,
+        Nebraska,
+        Nevada,
+        [Description("New Hampshire")]
+        NewHampshire,
+        [Description("New Jersey")]
+        NewJersey,
+        [Description("New Mexico")]
+        NewMexico,
+        [Description("New York")]
+        NewYork,
+        [Description("North Carolina")]
+        NorthCarolina,
+        [Description("North Dakota")]
+        NorthDakota,
+        Ohio,
+        Oklahoma,
+        Oregon,
+        Pennsylvania,
+        [Description("Rhode Island")]
+        RhodeIsland,
+        [Description("South Carolina")]
+        SouthCarolina,
+        [Description("South Dakota")]
+        SouthDakota,
+        Tennessee,
+        Texas,
+        Utah,
+        Vermont,
+        Virginia,
+        Washington,
+        [Description("West Virginia")]
+        WestVirginia,
+        Wisconsin,
+        Wyoming
+    }
+}
