a3a5a50b927f6d85edf709ed4006bef62379dd55
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 10:42:26 -0700

    Adding Customers page in UI

---
+++ assessment-api-developer.UI/Pages/Customers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace assessment_api_developer.UI.Pages
+{
+    public class CustomersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
