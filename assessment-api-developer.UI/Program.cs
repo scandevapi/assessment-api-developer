683c18b747bb1662e2aadcbe08c426ad642da421
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 16:29:56 -0700

    Preper UI project for getting data in Customers form

--- assessment-api-developer.UI/Program.cs
+++ assessment-api-developer.UI/Program.cs
@@ -1,7 +1,21 @@
+using assessment_api_developer.Domain.Interfaces;
+using assessment_api_developer.Infra.DataContext;
+using assessment_api_developer.Infra.Repositories;
+using assessment_api_developer.Services.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add DBContext Service
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+// Add Services
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 var app = builder.Build();
 
