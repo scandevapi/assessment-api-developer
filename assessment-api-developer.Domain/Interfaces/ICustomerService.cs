7a6c1929f98322d45716a6786cbf969c60a9644c
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 09:49:54 -0700

    Adding ICustomerService in domain

---
+++ assessment-api-developer.Domain/Interfaces/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using assessment_api_developer.Domain.Models;
+
+namespace assessment_api_developer.Domain.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<Customer> GetCustomerByIdAsync(int id);
+        Task<IEnumerable<Customer>> GetCustomersAsync();
+        Task<Customer> CreateCustomerAsync(Customer customer);
+        Task<Customer> UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(int id);
+    }
+}
