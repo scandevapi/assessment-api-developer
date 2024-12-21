efa9ebf48f3690d7f4f4e2c497c43861897c7998
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 11:22:25 -0700

    Adding methodes for validating Country,State,Zip in CustomerService

--- assessment-api-developer.Services/Services/CustomerService.cs
+++ assessment-api-developer.Services/Services/CustomerService.cs
@@ -1,6 +1,7 @@
 ﻿using assessment_api_developer.Domain.Exceptions;
 using assessment_api_developer.Domain.Interfaces;
 using assessment_api_developer.Domain.Models;
+using System.Text.RegularExpressions;
 
 namespace assessment_api_developer.Services.Services
 {
@@ -25,12 +26,14 @@ namespace assessment_api_developer.Services.Services
 
         public async Task AddCustomerAsync(Customer customer)
         {
+            ValidateCustomerStateAndZip(customer);
             await _customerRepository.AddAsync(customer);
         }
 
         public async Task UpdateCustomerAsync(Customer customer)
         {
             var existingCustomer = await EnsureCustomerExistsAsync(customer.ID);
+            ValidateCustomerStateAndZip(customer);
             await _customerRepository.UpdateAsync(customer);
         }
 
@@ -50,6 +53,71 @@ namespace assessment_api_developer.Services.Services
                 throw new CustomerNotFoundException($"Customer with id {customerId} not found");
             }
             return customer;
+        }
+
+
+        private void ValidateCustomerStateAndZip(Customer customer)
+        {
+            if (!string.IsNullOrEmpty(customer.State))
+            {
+                if (!IsValidStateOrProvince(customer.Country, customer.State))
+                {
+                    throw new Exception("Invalid state or province for the selected country.");
+                }
+                if (!IsValidateZipCode(customer.Country, customer.Zip))
+                {
+                    throw new Exception("Invalid zip code for the selected country.");
+                }
+            }
+        }
+
+
+        private bool IsValidStateOrProvince(string country, string stateOrProvince)
+        {
+            if (country == null || stateOrProvince == null)
+            {
+                return false;
+            }
+
+            if (!Enum.TryParse(country, out Countries parsedCountry))
+            {
+                return false;
+            }
+
+            if (parsedCountry == Countries.UnitedStates)
+            {
+                return Enum.TryParse(typeof(USStates), stateOrProvince, out _);
+            }
+            else if (parsedCountry == Countries.Canada)
+            {
+                return Enum.TryParse(typeof(CanadianProvinces), stateOrProvince, out _);
+            }
+
+            return false;
+        }
+
+        private bool IsValidateZipCode(string country, string zipCode)
+        {
+            if (country == null || zipCode == null)
+            {
+                return false;
+            }
+
+            if (!Enum.TryParse(country, out Countries parsedCountry))
+            {
+                return false;
+            }
+
+            if (parsedCountry == Countries.UnitedStates)
+            {
+                return Regex.IsMatch(zipCode, @"^\d{5}(-\d{4})?$");
+            }
+            if (parsedCountry == Countries.Canada)
+            {
+                return Regex.IsMatch(zipCode, @"^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$");
+            }
+
+            return false;
         }
     }
 }
