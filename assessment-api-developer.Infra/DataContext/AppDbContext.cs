b23f2687be91a0f9f74942f98e5196243d8aebd0
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 10:26:00 -0700

    Adding AppDbContext in Infra

---
+++ assessment-api-developer.Infra/DataContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using assessment_api_developer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace assessment_api_developer.Infra.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
