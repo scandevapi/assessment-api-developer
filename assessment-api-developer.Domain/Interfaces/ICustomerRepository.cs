0137bbce607aa8d05bbbbe14603431754b992a58
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 09:53:13 -0700

    Adding ICustomerRepository in Domain

---
+++ assessment-api-developer.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using assessment_api_developer.Domain.Models;
+
+namespace assessment_api_developer.Domain.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer> GetAsync(int id);
+        Task<Customer> AddAsync(Customer customer);
+        Task<Customer> UpdateAsync(Customer customer);
+        Task<Customer> DeleteAsync(int id);
+
+    }
+}
