af0e567d837c6845fbe334938289b96377e5e2a1
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 15:30:41 -0700

    Testing the CustomController Actions and fix issues on Add and update methods

---
+++ assessment-api-developer.Domain/Exceptions/CustomerExistException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace assessment_api_developer.Domain.Exceptions
+{
+    public class CustomerExistException : Exception
+    {
+        public CustomerExistException() : base() { }
+        public CustomerExistException(string message) : base(message) { }
+        public CustomerExistException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
--- assessment-api-developer.Infra/Repositories/CustomerRepository.cs
+++ assessment-api-developer.Infra/Repositories/CustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using assessment_api_developer.Domain.Interfaces;
+﻿using assessment_api_developer.Domain.Exceptions;
+using assessment_api_developer.Domain.Interfaces;
 using assessment_api_developer.Domain.Models;
 using assessment_api_developer.Infra.DataContext;
 using Microsoft.EntityFrameworkCore;
@@ -26,14 +27,23 @@ namespace assessment_api_developer.Infra.Repositories
 
         public async Task AddAsync(Customer customer)
         {
+            if (await _context.Customers.AnyAsync(c => c.Name == customer.Name))
+            {
+                throw new CustomerExistException($"Customer with Name {customer.Name} already exists.");
+            }
+
             await _context.Customers.AddAsync(customer);
             await _context.SaveChangesAsync();
         }
 
         public async Task UpdateAsync(Customer customer)
         {
-            _context.Customers.Update(customer);
-            await _context.SaveChangesAsync();
+            var existingCustomer = await _context.Customers.FindAsync(customer.ID);
+            if (existingCustomer != null)
+            {
+                _context.Entry(existingCustomer).CurrentValues.SetValues(customer);
+                await _context.SaveChangesAsync();
+            }  
         }
 
         public async Task DeleteAsync(int id)
