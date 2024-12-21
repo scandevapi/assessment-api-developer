607d1171829033348b6cf375950c2207aad26a06
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 13:17:08 -0700

    Add test for GetAllCustomers in ControllerTests

--- assessment-api-developer.Test/Controllers/CustomersControllerTests.cs
+++ assessment-api-developer.Test/Controllers/CustomersControllerTests.cs
@@ -1,11 +1,38 @@
+using assessment_api_developer.API.Controllers;
+using assessment_api_developer.Domain.Interfaces;
+using assessment_api_developer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
 namespace assessment_api_developer.Test.Services
 {
     public class CustomersControllerTests
     {
-        [Fact]
-        public void Test1()
+        private readonly Mock<ICustomerService> _mockService;
+        private readonly CustomersController _controller;
+
+        public CustomersControllerTests()
         {
+            _mockService = new Mock<ICustomerService>();
+            _controller = new CustomersController(_mockService.Object);
+        }
+
+        [Fact]
+        public async Task GetAllCustomers_ShouldReturnokObjectResult()
+        {
+            var customers = new List<Customer>
+            { 
+                new Customer {ID=1, Name = "Customer One"},
+                new Customer {ID=1, Name = "Customer Two"}
+            };
+            _mockService.Setup(s => s.GetAllCustomersAsync()).ReturnsAsync(customers);
+
+            var result = await _controller.GetAllCustomers() as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(customers, result.Value);
 
         }
+        
     }
 }
