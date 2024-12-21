352060bcf5c328ab4f6ce0797ff4a1286732387d
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 09:44:35 -0700

    Adding CanadianProvinces enum

---
+++ assessment-api-developer.Domain/Models/CanadianProvinces.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace assessment_api_developer.Domain.Models
+{
+    public enum CanadianProvinces
+    {
+        Alberta,
+        [Description("British Columbia")]
+        BritishColumbia,
+        Manitoba,
+        NewBrunswick,
+        [Description("Newfoundland and Labrador")]
+        NewfoundlandAndLabrador,
+        [Description("Northwest Territories")]
+        NovaScotia,
+        Ontario,
+        [Description("Prince Edward Island")]
+        PrinceEdwardIsland,
+        Quebec,
+        Saskatchewan,
+        [Description("Yukon")]
+        NorthwestTerritories,
+        Nunavut,
+        Yukon
+    }
+}
