5b0accf71f6abeb25c4d3f3c8ca550f6b3a76302
Mohammad Zamani <applicationd863@gmail.com>
2024-12-21 11:33:13 -0700

    Prepare Test project for testing CustomerService

--- assessment-api-developer.Test/Services/CustomerServiceTests.cs
+++ assessment-api-developer.Test/Services/CustomerServiceTests.cs
@@ -1,7 +1,20 @@
+using assessment_api_developer.Domain.Interfaces;
+using assessment_api_developer.Services.Services;
+using Moq;
+
 namespace assessment_api_developer.Test.Services
 {
     public class CustomerServiceTests
     {
+        private readonly Mock<ICustomerRepository> _mockRepository;
+        private readonly CustomerService _customerService;
+
+        public CustomerServiceTests()
+        {
+            _mockRepository = new Mock<ICustomerRepository>();
+            _customerService = new CustomerService(_mockRepository.Object);
+        }
+
         [Fact]
         public void Test1()
         {
