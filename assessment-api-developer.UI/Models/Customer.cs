263672b400266a9517464bc84f8382a5bdab9565
Mohammad Zamani <applicationd863@gmail.com>
2024-12-22 13:22:00 -0700

    Adding RegularExpression for supporting validate for US zip code and Canadian Postal code

--- assessment-api-developer.UI/Models/Customer.cs
+++ assessment-api-developer.UI/Models/Customer.cs
@@ -16,7 +16,7 @@ namespace assessment_api_developer.UI.Models
         public string? City { get; set; }
         public string? State { get; set; }
 
-        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid postal code format")]
+        [RegularExpression(@"(^\d{5}(-\d{4})?$)|(^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$)", ErrorMessage = "Invalid postal code format")]
         public string? Zip { get; set; }
         public string? Country { get; set; }
         public string? Notes { get; set; }
