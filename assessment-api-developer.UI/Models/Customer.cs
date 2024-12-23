2d522d0e464edddfe1f36737ecfd8644a876f5d9
Mohammad Zamani <applicationd863@gmail.com>
2024-12-23 10:23:15 -0700

    Adding RegularExpressions in Customer class on UI for data validation

--- assessment-api-developer.UI/Models/Customer.cs
+++ assessment-api-developer.UI/Models/Customer.cs
@@ -7,13 +7,19 @@ namespace assessment_api_developer.UI.Models
         public int ID { get; set; } = 0;
 
         [Required]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name can only contain letters and spaces.")]
         public string? Name { get; set; }
         public string? Address { get; set; }
 
-        [EmailAddress]
+        //[EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address format.")]
         public string? Email { get; set; }
+
+        [RegularExpression(@"^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$", ErrorMessage = "Invalid phone number format.")] /*(e.g., (123) 456-7890 or 123-456-7890)*/
         public string? Phone { get; set; }
         public string? City { get; set; }
+
+        [RegularExpression(@"^[A-Z][a-z]+(?:[A-Z][a-z]+)*$", ErrorMessage = "State must be a valid full name with each word capitalized and no spaces.")]
         public string? State { get; set; }
 
         [RegularExpression(@"(^\d{5}(-\d{4})?$)|(^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$)", ErrorMessage = "Invalid postal code format")]
@@ -21,9 +27,12 @@ namespace assessment_api_developer.UI.Models
         public string? Country { get; set; }
         public string? Notes { get; set; }
         public string? ContactName { get; set; }
+
+        [RegularExpression(@"^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$", ErrorMessage = "Invalid phone number format.")] /*(e.g., (123) 456-7890 or 123-456-7890)*/
         public string? ContactPhone { get; set; }
 
-        [EmailAddress]
+        //[EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email address format.")]
         public string? ContactEmail { get; set; }
         public string? ContactTitle { get; set; }
         public string? ContactNotes { get; set; }
