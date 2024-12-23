e5416e754df5cf661969816399c6f9f7603a15b0
Mohammad Zamani <applicationd863@gmail.com>
2024-12-22 17:05:46 -0700

    Remove js validation file from UI

--- assessment-api-developer.UI/wwwroot/js/validation.js
+++
@@ -1,9 +0,0 @@
-﻿document.getElementById('customerForm').addEventListener('submit', function (event) {
-    var state = document.getElementById('Customer_State').value;
-    var postalCode = document.getElementById('Customer_PostalCode').value;
-
-    if (state && !/^\d{5}(-\d{4})?$/.test(postalCode)) {
-        alert('Invalid postal code format for the provided state.');
-        event.preventDefault();
-    }
-});
