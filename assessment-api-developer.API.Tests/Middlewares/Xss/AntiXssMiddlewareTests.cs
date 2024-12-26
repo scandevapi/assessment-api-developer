36ba6722492c1894253f3bf8a65403683b26b933
Mohammad Zamani <applicationd863@gmail.com>
2024-12-25 23:51:22 -0700

    Fiexed SanitizesJsonRequestBody test issue in API.Tests

--- assessment-api-developer.API.Tests/Middlewares/Xss/AntiXssMiddlewareTests.cs
+++ assessment-api-developer.API.Tests/Middlewares/Xss/AntiXssMiddlewareTests.cs
@@ -23,31 +23,31 @@ namespace assessment_api_developer.API.Tests.Middlewares
 
 
 
-        //[Fact]
-        //public async Task InvokeAsync_SanitizesJsonRequestBody()
-        //{
-        //    // Arrange
-        //    var context = new DefaultHttpContext();
-        //    context.Request.ContentType = "application/json";
-        //    var originalBody = "{\"name\":\"<script>alert('xss')</script>\"}";
-        //    var sanitizedBody = "{\"name\":\"alert('xss')\"}";
+        [Fact]
+        public async Task InvokeAsync_SanitizesJsonRequestBody()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            context.Request.ContentType = "application/json";
+            var originalBody = "{\"name\":\"<script>alert('xss')</script>\"}";
+            var sanitizedBody = "{\"name\":\"\"}";
 
-        //    context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(originalBody));
-        //    context.Request.Body.Position = 0;
+            context.Request.Body = new MemoryStream(Encoding.UTF8.GetBytes(originalBody));
+            context.Request.Body.Position = 0;
 
-        //    // Act
-        //    await _middleware.InvokeAsync(context);
+            // Act
+            await _middleware.InvokeAsync(context);
 
-        //    // Assert
-        //    context.Request.Body.Position = 0;
-        //    using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8))
-        //    {
-        //        var body = await reader.ReadToEndAsync();
-        //        Assert.Equal(sanitizedBody, body);
-        //    }
+            // Assert
+            context.Request.Body.Position = 0;
+            using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8))
+            {
+                var body = await reader.ReadToEndAsync();
+                Assert.Equal(sanitizedBody, body);
+            }
 
-        //    _mockNext.Verify(next => next(context), Times.Once);
-        //}
+            _mockNext.Verify(next => next(context), Times.Once);
+        }
 
         [Fact]
         public async Task InvokeAsync_DoesNotSanitizeNonJsonRequestBody()
